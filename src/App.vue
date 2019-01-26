94009cb8393826cb84ffa052c6ab5b7d1923bbc9
Kir Kostin <GiMun0@gmail.com>
2019-01-26 14:33:27 +0300

    Затемнение при загрузки

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
     .page
+        .page__darkening(:class="{'_active': loadingQueue.length}")
         Header
         main.page__main
             transition(name="fade")
@@ -7,11 +8,17 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 import Header from '@/components/layout/Header';
 
 export default {
     components: { Header },
+
+    computed: {
+        ...mapState({
+            loadingQueue: state => state.loadingQueue,
+        }),
+    },
 
     created() {
         this.FETCH_POSTS();
@@ -58,4 +65,23 @@ body,
 
     &__main
         padding-top 70px
+
+    &__darkening
+        position fixed
+        top 0
+        left 0
+        right 0
+        bottom 0
+        z-index 99999
+        background-color rgba(0,0,0,.15)
+        display flex
+        align-items center
+        justify-content center
+        opacity 0
+        transition opacity .33s
+        visibility hidden
+
+        &._active
+            opacity 1
+            visibility visible
 </style>
